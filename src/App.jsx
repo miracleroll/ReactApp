777b1ba73406b7352b3921dd5a73a6c12d2b7862
miracleroll <mary-lukonina@yandex.ru>
2021-11-30 22:08:29 +0300

    lesson3

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import { useState, useEffect } from "react";
 import './App.css';
 import { FormMessage } from './Components/counter/FormMessage/FormMessage';
 import { MessageList } from './Components/counter/MessageList/MessageList';
+import {v4 as uuidv4} from "uuid";
+import { createTheme } from '@mui/material/styles';
+import { ChatList } from './Components/counter/Chats/Chats';
+import { Grid } from "@mui/material/";
+
 
 
 
 function App () {
 
   const INIT_MESSAGES = {
+    id: uuidv4(),
     author: "Бот",
     text: "Добро пожаловать в чат. Напиши мне что-нибудь",
 
 };
 
 const BOT_MESSAGES = {
+  id: "",
     author: "Бот",
     text: "Спасибо, что оставил сообщение!Ты классный!",
 };
@@ -23,9 +30,16 @@ const [messageList, setMessageList] = useState([INIT_MESSAGES]);
 
   useEffect(() => {
     let timer = null;
+    const botMessage = { id: "", author: "", text: "" };
     const lastMessage = messageList[messageList.length - 1];
     if (lastMessage.author !== "Бот")
       timer = setTimeout(() => {
+        if (BOT_MESSAGES.id === "") {
+          botMessage.id = uuidv4();
+          botMessage.author = BOT_MESSAGES.author;
+          botMessage.text = BOT_MESSAGES.text;
+        }
+
         setMessageList([...messageList, BOT_MESSAGES]);
       }, 3000);
 
@@ -35,18 +49,37 @@ const [messageList, setMessageList] = useState([INIT_MESSAGES]);
 
   }, [messageList]);
 
+  const theme = createTheme({
+      palette: {
+        primary: {
+          main: '#ec407a',
+        },
+        secondary: {
+          main: '#f48fb1',
+        },
+      },
+  });
+
   return (
-    <div className="App">
+    <div className="App" >
+      <Grid item xs={12} sm={4} md={3} lg={2} className="Chats">
+        <ChatList/>
+      </Grid>
+      <Grid item xs={12} sm={8} md={9} lg={10}>
       <header className="App-header">Чат</header>
-      <main className="App-main">
-        <MessageList messageList={messageList} />
+      <main>
+        <MessageList  messageList={messageList}/>
         <FormMessage
           messageList={messageList}
           setMessageList={setMessageList}
         />
       </main>
+      </Grid>
       <footer className="App-footer"></footer>
     </div>
+
+
+
   );
 }
 
--- src/Components/counter/FormMessage/FormMessage.jsx
+++ src/Components/counter/FormMessage/FormMessage.jsx
@@ -1,11 +1,40 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import './FormMessage.css';
+import { v4 as uuidv4 } from "uuid";
+import { TextField } from "@mui/material/";
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f381a7',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+  },
+});
+
+
 
 
 
 export function FormMessage({ messageList, setMessageList }) {
     const [messageAuthor, setMessageAuthor] = useState("");
     const [messageText, setMessageText] = useState("");
+
+    const [focus, ] = useState(true);
+    const inputRef = useRef(null);
+    useEffect(() => {
+      inputRef.current?.focus();
+    }, [messageText, focus]);
+
     const handleAuthorChange = (e) => setMessageAuthor(e.target.value);
     const handleTextChange = (e) => setMessageText(e.target.value);
     const handleAdd = (e) => {
@@ -14,6 +43,7 @@ export function FormMessage({ messageList, setMessageList }) {
       if (messageAuthor === "бот")
         return alert("вы не можете оставлять сообщения от этого имени");
         const newMessage = {
+          id: uuidv4(),
             author: messageAuthor,
             text: messageText,
           };
@@ -21,6 +51,9 @@ export function FormMessage({ messageList, setMessageList }) {
     setMessageText("");
     return setMessageList([...messageList, newMessage]);
 };
+
+
+
 return (
   <form
     className="message_form"
@@ -29,23 +62,32 @@ return (
       e.preventDefault();
     }}
   >
-    <input
-      className="message_form_author"
-      value={messageAuthor}
-      onChange={handleAuthorChange}
-      placeholder="Введите имя"
-      />
-      <textarea
-        className="message_form_text"
-        value={messageText}
-        onChange={handleTextChange}
-        ></textarea>
-      <input
-        className="message_form_submit"
-        type="submit"
-        onClick={handleAdd}
-        value="Отправить"
-      ></input>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="input-with-icon-adornment" >
+          Введите имя
+        </InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+            </InputAdornment>
+          }
+          onChange={handleAuthorChange}
+        />
+      </FormControl>
+        <TextField
+          id="outlined-textarea"
+          label="Написать сообщение"
+          multiline
+          value={messageText}
+          onChange={handleTextChange}
+          inputRef={inputRef}
+          size="small"
+          sx={{  m:"10px", flexGrow: 1 }}
+        />
+        <Stack spacing={2} direction="row">
+      <Button variant="contained" className="message_form_submit" theme={theme} onClick={handleAdd}>Отправить</Button>
+    </Stack>
     </form>
   );
 }
